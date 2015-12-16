d37e2937473436906bb1ddba0756c5e44070d343
BuisiX <nwabuisi33@gmail.com>
2015-12-16 13:31:50 -0600

    Assignment 43 markdown methods for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,18 @@ class Post < ActiveRecord::Base
   validates :body, length: {minimum: 20}, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  def markdown_title
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render title).html_safe
+  end
+
+  def markdown_body
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render body).html_safe
+  end
 end
